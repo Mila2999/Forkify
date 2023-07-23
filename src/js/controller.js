6c40402851c42e96e17d4b801f921f054008b73e
Mila Savonik <mila@MacBook-Pro-Mila.local>
2023-07-23 19:38:31 +0300

    create app

---
+++ src/js/controller.js
@@ -0,0 +1,54 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import View from './views/View.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpiner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Render recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpiner();
+
+    // 1) Get search guery
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRecipe(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
