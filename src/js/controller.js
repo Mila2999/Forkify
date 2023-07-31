2d0b948b72dd65bfaf2af2dd0723145537a1d476
Mila Savonik <mila@MacBook-Pro-Mila.local>
2023-07-31 17:05:34 +0300

    Implementing pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 import View from './views/View.js';
 
 import 'core-js/stable';
@@ -43,13 +44,23 @@ const controlSearchResults = async function () {
     // 3) Render results
     // resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultPage(goToPage));
 
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
 const init = function () {
   recipeView.addHandlerRecipe(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
